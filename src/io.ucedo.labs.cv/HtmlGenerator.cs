b1e862a61a5e1906748bc89f23be290d5277970b
Sebastián Ucedo <sebasucedo@gmail.com>
2023-07-20 20:23:52 -0300

    getting body from cache

--- src/io.ucedo.labs.cv/HtmlGenerator.cs
+++ src/io.ucedo.labs.cv/HtmlGenerator.cs
@@ -1,21 +1,74 @@
-﻿using DotLiquid;
+﻿using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using DotLiquid;
+using io.ucedo.labs.cv.domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Model;
+using Amazon.Lambda;
+using System.Net;
 
 namespace io.ucedo.labs.cv;
 
 public class HtmlGenerator
 {
     const string DATA_URL = "https://cdn.ucedo.io/cv/raw.json";
+    const string ENVIRONMENT_SOCKET_URL = "api_gateway_websocket";
+    const string ENVIRONMENT_ARN_AI_LAMBDA = "arn_ai_lambda";
 
-    public static async Task<string> GenerateHtml()
+    readonly CacheManager _cacheManager;
+
+    public HtmlGenerator()
+    {
+        _cacheManager = new CacheManager(new DynamoDBContext(new AmazonDynamoDBClient()));
+    }
+
+    public async Task<GenerateResponse> GenerateHtml(string key)
     {
         var html = await GetBasic();
 
+        var response = new GenerateResponse();
+        var body = await _cacheManager.Get(key);
+        if (string.IsNullOrEmpty(body))
+        {
+            body = await GetBody(key);
+            response.StatusCode = HttpStatusCode.Created;
+            await Generate(key);
+        }
+
+        html = SetBody(html, body);
+        response.Content = html;
+
+        return response;
+    }
+
+    private static async Task<string> GetBody(string queryString)
+    {
+        var socketUrl =  System.Environment.GetEnvironmentVariable(ENVIRONMENT_SOCKET_URL) ?? string.Empty;
+        var websocketUrl = socketUrl + (queryString == Constants.DEFAULT ? string.Empty : "?" + queryString);
+
+        string templateContent = await File.ReadAllTextAsync("./views/body.liquid");
+
+        Template template = Template.Parse(templateContent);
+        var templarParameters = Hash.FromAnonymousObject(new { queryString, websocketUrl });
+        var html = template.Render(templarParameters);
+
+        return html;
+    }
+
+    private static string SetBody(string html, string body)
+    {
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        HtmlNode bodyNode = doc.DocumentNode.SelectSingleNode("//body");
+        bodyNode.InnerHtml = body;
+        html = doc.DocumentNode.OuterHtml;
         return html;
     }
 
@@ -55,6 +108,33 @@ public class HtmlGenerator
         var html = template.Render(templarParameters);
 
         return html;
+    }
+
+    private static async Task Generate(string queryString)
+    {
+        var lambdaCliente = new AmazonLambdaClient();
+
+        var functionArn = System.Environment.GetEnvironmentVariable(ENVIRONMENT_ARN_AI_LAMBDA);
+        var payload = JsonSerializer.Serialize(queryString);
+
+        var invokeRequest = new InvokeRequest
+        {
+            FunctionName = functionArn,
+            InvocationType = InvocationType.Event,
+            Payload = payload,
+        };
+
+        try
+        {
+            var response = await lambdaCliente.InvokeAsync(invokeRequest);
+
+            if (response.HttpStatusCode != HttpStatusCode.Accepted)
+                LambdaLogger.Log($"Call to {functionArn} failed with status code: {response.HttpStatusCode}.");
+        }
+        catch (Exception ex)
+        {
+            LambdaLogger.Log($"Exception calling {functionArn} failed with message: {ex.Message}.");
+        }
     }
 
 }
