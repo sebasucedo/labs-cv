d5a522997e089f39879dec443a4d16239904cae1
Sebastián Ucedo <sebasucedo@gmail.com>
2023-07-21 21:56:37 -0300

    basic AI

---
+++ src/io.ucedo.labs.cv.ai/domain/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io.ucedo.labs.cv.ai.domain;
+
+public class Constants
+{
+    public const string DEFAULT = "default";
+    public const string DEFAULT_LANGUAGE = "english";
+    public const string SHRUGGIE = "¯_(ツ)_/¯";
+}
